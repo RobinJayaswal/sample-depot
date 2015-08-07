f8e1a91574d6278159a4556f16dab7e367adbdbf
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2015-08-07 12:05:19 -0700

    Add Product Price to Line Item Worked

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,12 +1,12 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
   
-  def add_line_item (product_id)
+  def add_line_item (product_id, product_price)
     item = line_items.find_by_product_id(product_id)
     if item
       item.quantity += 1
     else
-      item = line_items.build(product_id: product_id)
+      item = line_items.build(product_id: product_id, price: product_price)
     end
     item
   end
