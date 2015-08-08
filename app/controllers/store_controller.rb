0b491af968ea7148c87c06b212583875c23d0a89
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2015-08-07 17:22:56 -0700

    cartAddedToSidebar

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,7 +2,7 @@ class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
     @time = Time.now
-    
+    @cart = current_cart
     if session[:counter]
       session[:counter] += 1
     else
