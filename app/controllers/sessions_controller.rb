018f23e86b786484ff3b386267c347626676feef
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2015-08-21 20:39:01 -0700

    New attempt at letting admin bypass login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    logger.info "pressed create"
-    if User.count != 0
-      user = User.find_by_name(params[:name])
-      if user and user.authenticate(params[:password])
+    User.destroy_all
+    if User.count.zero?
+      user = User.create(name: params[:name], password: params[:password], password_confirmation: params[:password])
+    else
+      user = User.authenticate(name: params[:name], password: params[:password])
+    end
+    if user
         session[:user_id] = user.id
         redirect_to admins_url
-      else
-        redirect_to login_url, notice: "Incorrect Login"
-      end
     else
-      logger.info "User count is zero, sending to users controller"
-      redirect_to url_for(:controller => :users, :action => :create, :name => params[:name], :password => params[:password], :password_confirmation => params[:password])
+        redirect_to login_url, notice: "Incorrect Login"
     end
   end
 
